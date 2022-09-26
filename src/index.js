abd126509dfe6df7d714da1bd4e349baf18d4644
Glib Lakeev <glakeev@gmail.com>
2022-09-26 18:45:06 -0400

    finished basic slideshow, committing.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 
+//dropdown
 const button = document.getElementsByClassName("drop-down-button");
 
 button[0].addEventListener(`click`, function () {
@@ -19,11 +20,35 @@ window.onclick = function (e) {
   }
 };
 
+//hamburger menu:
 window.onload = function () {
   const menu_btn = document.querySelector(".hamburger");
   const mobile_menu = document.querySelector(".mobile-nav");
+
   menu_btn.addEventListener(`click`, function () {
     menu_btn.classList.toggle("is-active");
     mobile_menu.classList.toggle("is-active");
   });
 };
+
+// image slider
+const buttons = document.querySelectorAll("[data-carousel-button");
+const dots = document.querySelectorAll(".dot");
+
+buttons.forEach((button) => {
+  button.addEventListener(`click`, () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
